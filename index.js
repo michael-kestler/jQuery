07439f2291e4e6c9fc9f759513afbf38f00f4c0b
michael-kestler <michael.s.kestler@gmail.com>
2022-10-23 21:13:06 -0700

    Made several modifications to html page utilizing jquery methods

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-$("h1").css("color", "red");
+$("h1").addClass("big-title margin-50");
+
+$("button").html("<em>Click Me</>")
+
+$("h1").click(function () {
+
+  $("h1").css("color", "purple");
+})
+
+$("a").attr("href", "https://www.yahoo.com");
+
+
+// for(var i = 0; i<5; i++){
+//   document.querySelectorAll("button")[i].addEventListener("click", function() {
+//     document.querySelector("h1").style.color = "purple"
+//   });
+// }
+
+
+//below jquery accomplishes same thing commented out for loop above
+
+$("button").click(function() {
+  $("h1").css("color", "purple")
+});
+
+//do not need to do the for loop because jquery will go through all of the buttons
